fced909fdd46442219701fd3c96eb5ecf54edcfb
matte <matte@LAPTOP-FAR11H69>
2019-04-25 22:10:48 +0200

    Main update

--- G8/src/start/G8.java
+++ G8/src/start/G8.java
@@ -1,9 +1,19 @@
 package start;
 
+import org.antlr.runtime.ANTLRStringStream;
+import org.antlr.runtime.CommonTokenStream;
+
+import lexer.G8Lexer;
+import parser.G8Parser;
+
 public class G8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 ANTLRStringStream in = new ANTLRStringStream("12*(5-6)");
+	     G8Lexer lexer = new G8Lexer(in);
+	     CommonTokenStream tokens = new CommonTokenStream(lexer);
+	     G8Parser parser = new G8Parser(tokens);
+	     parser.begin();
 
 	}
 

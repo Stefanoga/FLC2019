1c7d491d68f93481053b482653e10e8bfe699b30
matte <matte@LAPTOP-FAR11H69>
2019-04-25 22:35:14 +0200

    main update

--- G8/src/start/G8.java
+++ G8/src/start/G8.java
@@ -10,7 +10,8 @@ import parser.G8Parser;
 public class G8 {
 
 	public static void main(String[] args) throws RecognitionException {
-		 ANTLRStringStream in = new ANTLRStringStream("12*(5-6)");
+		 String stringa = "TITLE prova DRAWSPACE WIDTH 6.0 DRAWSPACE HEIGTH 2.0 LINE: XSTART 3.0 YSTART 4.0 XEND 7.0 YEND 8.0";
+		 ANTLRStringStream in = new ANTLRStringStream(stringa);
 	     G8Lexer lexer = new G8Lexer(in);
 	     CommonTokenStream tokens = new CommonTokenStream(lexer);
 	     G8Parser parser = new G8Parser(tokens);
